405da9e2f51af2f40707c1ceccf7a93aa8014814
John Oncher <johnoncher@gmail.com>
2019-08-28 11:48:37 -0700

    tests done

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,16 @@ import "./App.css";
 
 const App: React.FC = () => {
   const [count, setCount] = useState(0);
+  function greeter(person: string) {
+    console.log("Hello, " + person);
+  }
+
+  const userArr = ["John", "Joanne", "Brandon", "Cathy"];
 
   return (
     <div className="container4">
+      {userArr.map(item => greeter(item))}
+
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
